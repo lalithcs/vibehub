802a33da09333f4c96215289416d999888c6ae52
Challa Saraswathi Lalith <121499856+lalithcs@users.noreply.github.com>
2024-09-20 09:55:52 +0530

    bug fix:#3

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -61,7 +61,7 @@ const PostCard = ({ post }: PostCardProps) => {
             </div>
 
             <img 
-            src={post.imageUrl || '/assets/icons/profile-plcaeholder/svg'}
+            src={post.imageUrl || '/assets/icons/profile-placeholder.svg'}
             className="post-car_img"
             alt="post image"
             />
